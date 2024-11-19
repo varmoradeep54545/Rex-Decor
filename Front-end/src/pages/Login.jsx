f279a4ba156ce3418b9feb59fb578a89a7f56475
Varmora deep <113370542+varmoradeep54545@users.noreply.github.com>
2024-11-19 09:16:31 +0530

    Update Login.jsx

--- Front-end/src/pages/Login.jsx
+++ Front-end/src/pages/Login.jsx
@@ -50,7 +50,7 @@ const Login = () => {
 
     try {
       const response = await axios.post(
-        "http://localhost:5000/api/auth/login",
+        "https://rex-decor-api.vercel.app/api/auth/login",
         { email, password }
       );
 
